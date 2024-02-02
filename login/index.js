16f5c85ce6c951541347e27b119a4688c649992c
Usman <104028868+Usman-2@users.noreply.github.com>
2024-02-02 09:05:10 -0500

    Update index.js

--- login/index.js
+++ login/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yourDatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://Usmany:Jannah2016$@marketplace.pqmdtu3.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const yourSchema = new mongoose.Schema({
   
