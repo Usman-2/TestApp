7bc8392a81413423944fa19db79876308730ac61
Usman <104028868+Usman-2@users.noreply.github.com>
2024-02-02 09:03:24 -0500

    Update index.js

--- login/index.js
+++ login/index.js
@@ -3,3 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yourDatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const yourSchema = new mongoose.Schema({
+  
+});
+
+const YourModel = mongoose.model('YourModel', yourSchema);
